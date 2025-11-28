066f6f16d84189f321b4123960645e0a077d42a4
danycaste1304 <danycaste1304@gmail.com>
2025-11-28 13:41:12 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -56,9 +56,9 @@ function App() {
       lazo:     { top: "40px",                // NUEVO: subido a la cabeza
         width: "100px",             // NUEVO: tamaño más natural
         transform: "translateX(-60px)"},
-      guitarra: { top: "200px",                // NUEVO: subido a la cabeza
-        width: "200px",             // NUEVO: tamaño más natural
-        transform: "translateX(-50px)" },
+      guitarra: { top: "180px",                // NUEVO: subido a la cabeza
+        width: "170px",             // NUEVO: tamaño más natural
+        transform: "translateX(-60px)" },
     },
 
     conejo: {
