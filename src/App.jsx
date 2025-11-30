66de59fd6304dfabdbc25ee349b383d34a372c85
danycaste1304 <danycaste1304@gmail.com>
2025-11-30 12:23:32 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
         transform: "translateX(-50%)",
       },
       diadema: {
-        top: "15%",
+        top: "20%",
         left: "60%",
         width: "80%",
         transform: "translateX(-50%)",
